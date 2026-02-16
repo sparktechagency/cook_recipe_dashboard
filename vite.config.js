b80a4f049a0f79e28e3e590ad2555b2f357b08e3
Nurulla Hasan (Golap) <85323050+nurulla-hasan@users.noreply.github.com>
2026-02-16 10:19:21 +0600

    Update Vite config to include Tailwind CSS
    Added Tailwind CSS support and configured server settings.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,60 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   // server: {
+//   //   host: "0.0.0.0",
+//   //   port: "8002",
+//   // },
+  
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'url'
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
-  plugins: [react()],
-  // server: {
-  //   host: "0.0.0.0",
-  //   port: "8002",
-  // },
-  
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+
+  // DEV SERVER (npm run dev)
+  server: {
+    host: '0.0.0.0',
+    port: 5174,
+    strictPort: true,
+  },
+
+  // PREVIEW SERVER (npm run preview)
+  preview: {
+    host: '0.0.0.0',
+    port: 5174,
+    allowedHosts: [
+      'dashboard.koumanisdietapp.com',
+      'www.dashboard.koumanisdietapp.com',
+    ],
+  },
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+    extensions: [
+      '.mjs',
+      '.js',
+      '.mts',
+      '.ts',
+      '.jsx',
+      '.tsx',
+      '.json',
+    ],
+  },
 })
