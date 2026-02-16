9db626283263f2c7c46703aacb3fc10a8506902a
Osman Goni <94566411+goni715@users.noreply.github.com>
2026-02-16 10:29:52 +0600

    Disable tailwindcss plugin in vite.config.js
    Comment out the tailwindcss plugin in Vite config

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
+//import tailwindcss from '@tailwindcss/vite'
 import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
@@ -23,7 +23,7 @@ const __dirname = path.dirname(__filename)
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
+    //tailwindcss(),
   ],
 
   // DEV SERVER (npm run dev)
